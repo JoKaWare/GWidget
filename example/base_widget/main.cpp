248db597fbf62aace754740478ba7b753899c451
zhjw <zhjw@phillip.com>
2013-09-03 15:51:23 +0800

    r197479

---
+++ example/base_widget/main.cpp
@@ -0,0 +1,45 @@
+
+#include <tchar.h>
+#include <windows.h>
+#include <initguid.h>
+#include <oleacc.h>
+#include <atlbase.h>
+#include "base/command_line.h"
+#include "ui/base/win/scoped_ole_initializer.h"
+
+#include "demo_main.h"
+
+
+CComModule _Module;
+
+// 程序入口.
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                       HINSTANCE hPrevInstance,
+                       LPTSTR    lpCmdLine,
+                       int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+	ui::ScopedOleInitializer OleIniter;
+	CommandLine::Init(0, NULL);
+	DemoMain main;
+	main.Run();
+
+
+    return 0;
+}
+
+
+// 提升公共控件样式.
+#ifdef _UNICODE
+#if defined _M_IX86
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#elif defined _M_IA64
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='ia64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#elif defined _M_X64
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#else
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#endif
+#endif
